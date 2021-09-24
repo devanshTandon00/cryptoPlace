6d6ebe13b10d87b4f418dc6e6c7156f471db9106
tandond <devansh.tandon@cdk.com>
2021-09-24 08:32:06 -0700

    updated home page

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,41 +1,41 @@
-import React, { Component } from 'react';
-import './Navbar.css'
-import Input from './Input';
+import React, { Component } from "react";
+import "./Navbar.css";
+import Input from "./Input";
 
 export default class Navbar extends Component {
-  state = {}
+  state = {};
   render() {
     return (
-      <div className='navbar-container'>
-        <div className='navbar-logo'>
-          <a href='/' className='options-text'>
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <a href="/" className="options-text">
             CryptoPlace
           </a>
         </div>
         <Input
-          type='navbar-search'
-          placeholder='Search collectibles and accounts'
+          type="navbar-search"
+          placeholder="Search collectibles and accounts"
         />
-        <div className='navbar-options'>
-          <a href='/blog-page' className='options-text'>
+        <div className="navbar-options">
+          <a href="/blog-page" className="options-text">
             Blog Page
           </a>
-          <a href='/browse' className='options-text'>
+          <a href="/browse" className="options-text">
             Browse
           </a>
-          <a href='/create-collectible' className='options-text'>
+          <a href="/create-collectible" className="options-text">
             Create Collectible
           </a>
-          <a href='/manage-account' className='options-text'>
+          <a href="/manage-account" className="options-text">
             Manage Account
           </a>
-          <a href='/register' className='options-text'>
-            R
+          <a href="/register" className="options-text">
+            Register
           </a>
-          <a href='/login' className='options-text'>
-            L
+          <a href="/login" className="options-text">
+            Login
           </a>
-          <a href='/profile'>
+          <a href="/profile">
             <img className="profile-img" alt="unable to load" />
           </a>
         </div>
--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Jumbotron } from "reactstrap";
 
-
 export default function HomePage() {
   return (
     <div>
       <Jumbotron>
-        <h1>Welcome!</h1>
+        <h4>
+          <center>
+            Welcome to our marketplace - <h1>CryptoPlace 🚀</h1>
+          </center>
+        </h4>
       </Jumbotron>
     </div>
   );
