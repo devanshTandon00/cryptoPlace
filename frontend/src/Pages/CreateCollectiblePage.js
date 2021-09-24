7e9e30779e0f2074fe6469be0f0eae247459676e
Jason Huynh <63384364+TheNMEArengee@users.noreply.github.com>
2021-09-23 23:25:36 -0700

    Update CreateCollectiblePage.js (#2)

--- frontend/src/Pages/CreateCollectiblePage.js
+++ frontend/src/Pages/CreateCollectiblePage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import { Avatar } from '@material-ui/core';
 import Banner from '../Components/Banner';
 import Button from '../Components/Button';
 import Input from '../Components/Input';
 import Label from '../Components/Label';
+import './ManageAccountPage.css';
 
 
 export default class CreateCollectiblePage extends Component {
@@ -18,40 +20,49 @@ export default class CreateCollectiblePage extends Component {
 
                 <Banner title='Create Collectible' />
 
-                {/* <form className='CreateCollectiblePage' noValidate autoComplete="off">
-                    <TextField id="standard-basic" label="Standard" />
-                    <TextField id="filled-basic" label="Filled" variant="filled" />
-                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-                </form> */}
 
-                {/* <Avatar
-                    style={{
-                        margin: 'auto',
-                        width: '100px',
-                        height: '100px',
-                        backgroundColor: 'black'
-                    }}
-                    src='placeholder'
-                /> */}
-                <form>
-                    <Label title='Name' />
-                    <Input />
-                    <div style={{ marginBottom: '25px' }} />
-                    <Label title='Description' />
-                    <Input />
-                    <div style={{ marginBottom: '25px' }} />
-                </form>
-                <Button
-                    onClick={() => { console.log('hi') }}
-                    label={'Submit'}
-                />
 
+                <div className='manage-account-content'>
+
+                    <Avatar
+                        style={{
+                            margin: 'auto',
+                            width: '100px',
+                            height: '100px',
+                            backgroundColor: 'black'
+                        }}
+                        src='placeholder'
+                    />
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        <button
+                            style={{
+                                color: '#bababa',
+                                fontFamily: 'DM Sans, sans-serif',
+                                alignItems: 'center',
+                                marginTop: '2px',
+                                backgroundColor: 'white',
+                                border: 'none'
+                            }}
+                            onClick={() => console.log("hi")}
+                        >
+                            Edit
+                        </button>
+                    </div>
+                    <form>
+                        <Label title='Name' />
+                        <Input />
+                        <div style={{ marginBottom: '25px' }} />
+                        <Label title='Description' />
+                        <Input />
+                        <div style={{ marginBottom: '25px' }} />
+                    </form>
+                    <Button
+                        onClick={() => { console.log('hi') }}
+                        label={'Submit'}
+                    />
+
+                </div>
             </div>
-
-
-
-
-
         );
     }
 }
